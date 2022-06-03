7e721ad68e456cc1121f87bf7fc819ff579a4633
Abhigna-Nadupalli <nabhigna2909@gmail.com>
2022-06-03 19:22:30 +0530

    added footer with small changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,16 @@ import './Footer.css'
 const Footer = () => (
   <div className="footer">
      
-    <p>This is the footer</p>
+     <div bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+        <span>Connect with us on our social networks:</span>
+        </div>
+      </section>
+      </div>
    
   </div>
 );
 
 export default Footer;
+
