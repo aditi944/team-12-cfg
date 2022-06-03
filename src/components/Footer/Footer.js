a27f1d107483d30dd4a740fe6e9d2172b6ba97e8
aditi944 <aditijha201200@gmail.com>
2022-06-03 20:55:02 +0530

    a

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,41 @@
 import React from "react";
 // import { FaInstagram, FaFacebook, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaPinterest
+} from "react-icons/fa";
 import './Footer.css'
-const Footer = () => (
-  <div className="footer">
-     
-     <div bgColor='light' className='text-center text-lg-start text-muted'>
-      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
-        <span>Connect with us on our social networks:</span>
-        </div>
-      </section>
-      </div>
+let circleClasses = "inline-block p-7 rounded-full w-20 mx-auto";
+let iconStyles = { color: "white", fontSize: "1.5em" };
+
+const Footer = () => {
+ 
+  return (
+    
+    <div className="App grid grid-cols-2 sm:grid-cols-4 gap-2 w-3/4 mx-auto">
+      <h4>Connect with us on our social networks:</h4>
+      <span style={{ background: "#3B5998" }} className={circleClasses}>
+        <FaFacebookF style={iconStyles} />
+      </span>
+      <span style={{ background: "#1DA1F2" }} className={circleClasses}>
+        <FaTwitter style={iconStyles} />
+      </span>
+      <span style={{ background: "black" }} className={circleClasses}>
+        <FaInstagram style={iconStyles} />
+      </span>
+      <span style={{ background: "#BD081C" }} className={circleClasses}>
+        <FaPinterest style={iconStyles} />
+      </span>
+    </div>
+ 
    
-  </div>
-);
+ 
+  );
+  }
+
+  
 
 export default Footer;
 
